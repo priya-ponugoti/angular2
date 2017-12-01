e700a80e5b479f39c774fcdda53a76328fe2445c
priya-ponugoti <pxp21280@gmail.com>
2017-12-01 10:54:27 -0600

    Add files via upload

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title: string = 'Employee details';
+  imagepath: string ='https://angular.io/assets/images/logos/angular/logo-nav@2x.png';
+}
